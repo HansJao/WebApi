597a8eb75b4775426b93ed74cdf1dd6ac793149b
HansJao <vigorhan@gmail.com>
2018-01-09 11:01:30 +0800

    測試看看

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+using System.Web;
+using System.IO;
 
 namespace WebApi.Controllers
 {
@@ -13,8 +16,9 @@ namespace WebApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            
           isRock.LineBot.Utility.PushMessage(
-              "Udb332b1f0570d0d9258c5961a6300554", "要傳送的訊息testtest", "2SADvCCr30z0fFRLIFwkDirXsgT1Q8M0YskXowd9LpCRq3W5TwsatqfDFQi57i3Xoys8DElYuy29lx76Xl6/+hQHdjbX2tGtsW5p0dK14UMHaBI0jXXDt7dvAEZABGYnYoAm92tUWBfGTzOMf38YPwdB04t89/1O/w1cDnyilFU=");  
+              "Udb332b1f0570d0d9258c5961a6300554", "要傳送的訊息testtest", "MN840PMoy1tFbzfSVyoF51rG8vujtBRPHNf3m6r6JcPhJ2gicgwJD2gxM3vDpTK1oys8DElYuy29lx76Xl6/+hQHdjbX2tGtsW5p0dK14UP7A2vL24rIibhvqHiI3fgD6Vmixh7LYMCwPPiWjLBTowdB04t89/1O/w1cDnyilFU=");  
             return new string[] { "value1", "value2" };
         }
 
@@ -41,6 +45,37 @@ namespace WebApi.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        [HttpPost]
+        [Route("TestPost")]
+        public IActionResult TestPost()
+        {
+            string ChannelAccessToken = "MN840PMoy1tFbzfSVyoF51rG8vujtBRPHNf3m6r6JcPhJ2gicgwJD2gxM3vDpTK1oys8DElYuy29lx76Xl6/+hQHdjbX2tGtsW5p0dK14UP7A2vL24rIibhvqHiI3fgD6Vmixh7LYMCwPPiWjLBTowdB04t89/1O/w1cDnyilFU=";
+            string postData = string.Empty;
+            try
+            {
+                using (StreamReader reader = new StreamReader(Request.Body))
+            {
+                postData = reader.ReadToEnd();
+            }
+                //取得 http Post RawData(should be JSON)
+                //postData = Request.Body.ReadAsync;//Request.Content.ReadAsStringAsync().Result;
+                //剖析JSON
+                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                //回覆訊息
+                string Message;
+                Message = "你說了:" + ReceivedMessage.events[0].message.text;
+                //回覆用戶
+                isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
+                //回覆API OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                var test = ex;
+                return Ok();
+            }
         }
     }
 }
